8fc62cda45ca0579fdf37bc400238535d561c82f
lupovvladimir <vl.lupov.dev@gmail.com>
2022-09-19 01:50:27 +0300

    Added scanner

--- src/Home_work_monobank/Methods.java
+++ src/Home_work_monobank/Methods.java
@@ -12,4 +12,20 @@ public class Methods {
     static int term;
     static double depositAmount;
     static String depositAmountRounded;
+
+
+    static void doInputs() {
+
+        System.out.println("Enter your first name:");
+        scanner = new Scanner(System.in);
+        firstName = scanner.nextLine();
+
+        System.out.println("Enter your second name:");
+        scanner = new Scanner(System.in);
+        secondName = scanner.nextLine();
+
+        System.out.println("Enter the amount you want to put on deposit:");
+        scanner = new Scanner(System.in);
+        contributionAmount = scanner.nextDouble();
+    }
 }
