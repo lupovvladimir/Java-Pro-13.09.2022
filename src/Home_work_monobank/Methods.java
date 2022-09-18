d50c6cb8d60515170dd6cd130545ea1cca1f5b39
lupovvladimir <vl.lupov.dev@gmail.com>
2022-09-19 01:48:21 +0300

    Сreated a class Methods, added variables

---
+++ src/Home_work_monobank/Methods.java
@@ -0,0 +1,15 @@
+package Home_work_monobank;
+
+import java.util.Scanner;
+
+public class Methods {
+
+    static Scanner scanner;
+    static String firstName;
+    static String secondName;
+    static double contributionAmount;
+    static double rateOfInterest;
+    static int term;
+    static double depositAmount;
+    static String depositAmountRounded;
+}
