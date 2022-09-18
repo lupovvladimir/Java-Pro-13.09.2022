a292dfdba697e38da53a5031285b182e3c8d1755
lupovvladimir <vl.lupov.dev@gmail.com>
2022-09-19 01:56:44 +0300

    Added calculation for other deposits

--- src/Home_work_monobank/Methods.java
+++ src/Home_work_monobank/Methods.java
@@ -33,13 +33,26 @@ public class Methods {
         doInputs();
         rateOfInterest = 4.7;
         DepositForOneYear();
-
+        DepositForFiveYears();
+        DepositForTenYears();
     }
 
     private static void DepositForOneYear() {
         term = 365;
         depositAmount = countDeposit(contributionAmount, rateOfInterest, term);
         System.out.println("If you open a deposit for one year your deposit amount will be equal to - " + depositAmountRounded + "$ ");
+    }
+
+    private static void DepositForFiveYears() {
+        term = 365 * 5;
+        depositAmount = countDeposit(contributionAmount, rateOfInterest, term);
+        System.out.println("If you open a deposit for five years yours deposit amount will be equal to - " + depositAmountRounded + "$ ");
+    }
+
+    private static void DepositForTenYears() {
+        term = 365 * 10;
+        depositAmount = countDeposit(contributionAmount, rateOfInterest, term);
+        System.out.println("If you open a deposit for ten years yours deposit amount will be equal to - " + depositAmountRounded + "$ ");
     }
 
     static double countDeposit(double contributionAmount, double rateOfInterest, int term) {
