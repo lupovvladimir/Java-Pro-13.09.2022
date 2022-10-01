c1c3dafa01c024128598543fc1758044c12614bb
lupovvladimir <vl.lupov.dev@gmail.com>
2022-10-02 02:38:35 +0300

    Added classes

---
+++ src/Home_work_6/Car.java
@@ -0,0 +1,24 @@
+package Home_work_6;
+
+public class Car {
+
+    public void start() {
+        startCommand();
+        startElectricity();
+        startFuelSystem();
+
+    }
+
+    private static void startElectricity() {
+
+    }
+
+    private static void startCommand() {
+
+    }
+
+    private static void startFuelSystem() {
+
+    }
+
+}
