4e4892af96fd72c2fb340f61c9ba76b313d1be65
lupovvladimir <vl.lupov.dev@gmail.com>
2022-10-02 01:44:47 +0300

    Сhanged Method, variables and method names

--- src/Home_work_5/ConsoleCalculator.java
+++ src/Home_work_5/ConsoleCalculator.java
@@ -5,29 +5,26 @@ import java.util.Scanner;
 public class ConsoleCalculator {
 
     static Scanner scanner;
-    static double operandOne;
-    static double operandTwo;
+    static double operandFirst;
+    static double operandSecond;
 
     public static void main(String[] args) {
 
         System.out.println("Task one:");
-        String str = "юукяД ібот ,ежоб ощ я ен !ьлаксом";
+        String sentence = "ZюукяДZіботZ,ежобZощZяZенZ!ьлаксом";
 
-        flipString(str);
+        flipEachWordInStr(sentence);
         calculator();
 
     }
 
-    private static void flipString(String str) {
-        String[] strArray = str.split(" ");
-        for (int i = 0; i < 7; i++) {
-            char[] s1 = strArray[i].toCharArray();
-            for (int j = s1.length - 1; j >= 0; j--) {
-                System.out.print(s1[j]);
-            }
-            System.out.print(" ");
-        }
+    private static void flipEachWordInStr(String sentence) {
 
+        String[] words = sentence.split("Z");
+
+        for (String word : words) {
+            System.out.print(new StringBuilder(word).reverse() + " ");
+        }
     }
 
     private static void calculator() {
@@ -36,20 +33,20 @@ public class ConsoleCalculator {
         System.out.println("Console calculator with basic functionality greets you !");
         System.out.println("Enter first number:");
         scanner = new Scanner(System.in);
-        operandOne = scanner.nextDouble();
+        operandFirst = scanner.nextDouble();
 
         System.out.println("Select arithmetic operations (+, -, *, /):");
         char operation = scanner.next().charAt(0);
 
         System.out.println("Enter second number:");
         scanner = new Scanner(System.in);
-        operandTwo = scanner.nextDouble();
+        operandSecond = scanner.nextDouble();
 
         switch (operation) {
-            case '+' -> System.out.println("Result: " + add(operandOne, operandTwo));
-            case '-' -> System.out.println("Result: " + subtract(operandOne, operandTwo));
-            case '*' -> System.out.println("Result: " + multiply(operandOne, operandTwo));
-            case '/' -> System.out.println("Result: " + divide(operandOne, operandTwo));
+            case '+' -> System.out.println("Result: " + add(operandFirst, operandSecond));
+            case '-' -> System.out.println("Result: " + subtract(operandFirst, operandSecond));
+            case '*' -> System.out.println("Result: " + multiply(operandFirst, operandSecond));
+            case '/' -> System.out.println("Result: " + divide(operandFirst, operandSecond));
             default -> System.out.println("Not a correct character");
         }
 
